a071de54aca3d9159d4b94d90c229b3b859c4e07
Mattesu76 <m.bapaume@free.fr>
2017-05-11 20:14:16 +0200

    refonte solution

---
+++ WPF/VideoCaptureApplication.Utils/Constants/AppConstants.cs
@@ -0,0 +1,9 @@
+﻿
+namespace VideoCaptureApplication.Utils.Constants
+{
+    public class AppConstants
+    {
+        public const string FilesFolder = @"DataFile";
+        public const string FileName = @"parameters.json";
+    }
+}
---
+++ WPF/VideoCaptureApplication/Models/Language.cs
@@ -0,0 +1,12 @@
+﻿
+namespace VideoCaptureApplication.Models
+{
+    public class Language
+    {
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
