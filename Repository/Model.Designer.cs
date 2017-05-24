612efe22b8167d3e8b96eca07684b40cd9bba091
Violaine Michon <v.michon@cotral.com>
2017-05-24 14:17:12 +0200

    modif bDD et requte GetUser

--- Repository/Model.Designer.cs
+++ Repository/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La génération de code T4 est activée pour le modèle 'C:\Users\v.michon\Desktop\Cours MS2I\NET\CciFormationNet\Repository\Model.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'C:\Users\v.michon\Desktop\CoursMS2I\NET\CciFormationNet\Repository\Model.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
--- WebAPI/Controllers/UserController.cs
+++ WebAPI/Controllers/UserController.cs
@@ -11,7 +11,7 @@ namespace WebAPI.Controllers
 {
     public class UserController : ApiController
     {
-        public USER GetUsers(long login, string password)
+        public long GetUsers(string login, string password)
         {
             return UserService.GetUsers(login, password);
         }
