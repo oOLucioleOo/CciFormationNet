0260a2bde9b04aadc0551651662351bda552b7e3
Wrath1er <legrixaymeric@hotmail.fr>
2017-05-31 12:10:15 +0200

    correction erreur dll

--- WebAPI/Controllers/UserController.cs
+++ WebAPI/Controllers/UserController.cs
@@ -1,13 +1,7 @@
 using Entity;
 using Services;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
-using System.Web.Mvc;
 
 namespace WebAPI.Controllers
 {
