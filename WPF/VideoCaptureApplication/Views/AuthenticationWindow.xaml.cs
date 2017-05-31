bfed3d41a901a540b66d22d81e85b882bae2ecde
Arnaud <ticalfullauto@hotmail.com>
2017-05-31 12:17:31 +0200

    Update 31/05/2017 arnaud - authentification evol

--- WPF/VideoCaptureApplication/Views/AuthenticationWindow.xaml.cs
+++ WPF/VideoCaptureApplication/Views/AuthenticationWindow.xaml.cs
@@ -2,6 +2,11 @@
 using System.Windows;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Entity;
+
 
 namespace VideoCaptureApplication.Views
 {
@@ -13,33 +18,79 @@ namespace VideoCaptureApplication.Views
             InitializeComponent();
         }
 
-        private void BtnAuthOk_Click(object sender, RoutedEventArgs e)
+        private async void BtnAuthOk_Click(object sender, RoutedEventArgs e)
         {
-            //Initialize HTTP Client 
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://localhost:63315");
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            ////Initialize HTTP Client 
+            //HttpClient client = new HttpClient();
+            //client.BaseAddress = new Uri("http://localhost:63315");
+
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //MessageBox.Show("1");
+            //try
+            //{
+            //    var multipart = new MultipartFormDataContent();
+            //    MessageBox.Show("2");
+            //    multipart.Add(new StringContent(LoginTextBox.Text), "login");
+            //    multipart.Add(new StringContent(PwdTextBox.Password), "password");
+            //    MessageBox.Show("3");
+            //    var response = await client.PostAsync("http://localhost:63315/api/user/GetUsers/", multipart);
+            //    response.EnsureSuccessStatusCode(); // Throw on error code. 
+            //    MessageBox.Show("Bonjour Esclave N°1239821381723");
+            //    MessageBox.Show("4");
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show("Utilisateur Inconnu");
+            //}
+
+            HttpClient httpClient = new HttpClient();
             try
             {
-                var multipart = new MultipartFormDataContent();
-
-                multipart.Add(new StringContent(LoginTextBox.Text), "Login");
-                multipart.Add(new StringContent(PwdTextBox.Password), "pwd");
-
-                var response = client.PostAsync("/api/user/GetUsers", multipart).Result;
-                response.EnsureSuccessStatusCode(); // Throw on error code. 
-                MessageBox.Show("Bonjour Esclave N°1239821381723");
+                string resourceAddress = "http://localhost:63315/api/user/GetUsers/";
+                USER user = new USER { USER_LOG = this.LoginTextBox.Text, USER_PWD = this.PwdTextBox.Password};
+                httpClient.Timeout = TimeSpan.FromMilliseconds(100000);
+                httpClient.DefaultRequestHeaders.Add("ContentType", "application/json; charser = utf-8");
+                HttpResponseMessage wcfResponse = await httpClient.PostAsJsonAsync(resourceAddress, user);
             }
-
+            catch (HttpRequestException hre)
+            {
+                MessageBox.Show("Error:" + hre.Message);
+            }
+            catch (TaskCanceledException)
+            {
+                MessageBox.Show("Request canceled.");
+            }
             catch (Exception ex)
             {
-                MessageBox.Show("Utilisateur Inconnu");
+                MessageBox.Show(ex.Message);
             }
+            finally
+            {
+                if (httpClient != null)
+                {
+                    httpClient.Dispose();
+                    httpClient = null;
+                }
+            }
+
+
         }
 
         private void BtnAuthExit_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        public async Task<IApiResponse> ReadResponse(HttpResponseMessage message)
+        {
+            var response = new ApiResponse();
+            response.statusCode = (int)message.StatusCode;
+            if (response.StatusCode == 200)
+                response.Data = await message.Content.ReadAsStringAsync();
+            else
+                response.Message = await message.Content.ReadAsStringAsync();
+            return response;
+
         }
     }
 }
--- WebAPI/Controllers/UserController.cs
+++ WebAPI/Controllers/UserController.cs
@@ -2,6 +2,7 @@ using Entity;
 using Services;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -11,9 +12,18 @@ namespace WebAPI.Controllers
 {
     public class UserController : ApiController
     {
+        [Route("api/user/GetUsers/")]
+        [HttpPost]
         public long GetUsers(string login, string password)
         {
             return UserService.GetUsers(login, password);
+        }
+
+        [HttpGet]
+        public string getuser()
+        {
+            Debug.WriteLine("coucou");
+            return "coucou";
         }
     }
 }
